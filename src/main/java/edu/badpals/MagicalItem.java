a2c7d5442e3799d9f196b8ced7c5e9af63cb1861
Joel <joelfigueiridomolares@gmail.com>
2024-06-03 10:26:56 +0200

    Refactor(MagicalItem):Insertar metodos de Lombok

--- src/main/java/edu/badpals/MagicalItem.java
+++ src/main/java/edu/badpals/MagicalItem.java
@@ -2,56 +2,35 @@ package edu.badpals;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "t_items")
 public class MagicalItem extends PanacheEntityBase {
-    public MagicalItem() {
-    }
-    public MagicalItem(String name, int quality, String tipo) {
-        this.name = name;
-        this.quality = quality;
-        this.tipo = tipo;
-    }
-    @Id
-    @Column(name = "item_id")
-    public Long id=0L;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "item_id")
+	public @Getter long id;
 
-    @Column(name = " item_name")
-    public String name ="";
+    @Column(name="item_name")
+	@NotEmpty
+	private @Getter @Setter String name;
 
-    @Column(name = "item_quality")
-    public int quality = 0;
+    @Column(name="item_quality")
+	@NotNull
+	private @Getter @Setter int quality;
 
     @Column(name = "item_type")
-    public String tipo="";
+	@NotEmpty
+	private @Getter @Setter String type;
 
-    public Long getId(){
-        return id;
-    }
-
-    public String getName(){
-        return name;
-    }
-
-    public int getQuality(){
-        return quality;
-    }
-
-    public String getType(){
-        return tipo;
-    }
-    public void setName(String name){
-        this.name = name;
-    }
-    public void setQuality(int quality){
-        this.quality = quality;
-    }
-    public void setType(String tipo){
-        this.tipo = tipo;
-    }
+   
     @Override
     public String toString() {
         StringBuilder itm = new StringBuilder();
@@ -59,7 +38,7 @@ public class MagicalItem extends PanacheEntityBase {
             .append("id=").append(id)
             .append(", name='").append(name).append('\'')
             .append(", quality=").append(quality)
-            .append(", tipo='").append(tipo).append('\'')
+            .append(", tipo='").append(type).append('\'')
             .append('}');
         return itm.toString();
     }
